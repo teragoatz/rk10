e295f8b13d9c939b4cf72fad225f75221ae82220
reigningdev <reigningdev@proton.me>
2025-09-15 15:48:09 -0700

    remove console log

--- client/src/hooks/useGetTournamentList.ts
+++ client/src/hooks/useGetTournamentList.ts
@@ -3,8 +3,6 @@ import axios from '../api';
 import { Tournament } from './useGetTournament';
 import { SERVER_URI } from '../util';
 
-console.log(SERVER_URI);
-
 async function getTournamentList(status: string): Promise<Tournament[]> {
   const response = await axios.get(`${SERVER_URI}/api/tournaments?status=${status}`);
   return response.data;
